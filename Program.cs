1a720fa8f9a6ca899718e53c5333a7f0071da82a
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-02-24 11:08:24 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMusicDistr.Data;
+using MvcMusicDistr.Models;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MvcMusicDistrContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMusicDistrContext") ?? throw new InvalidOperationException("Connection string 'MvcMusicDistrContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+
+    SeedDataG.Initialize(services);
+    SeedDataP.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();

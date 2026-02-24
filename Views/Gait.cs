5943d760238788d1ffa3d601c55f3a58fc907d51
Zubkov Ruslan <not.hapee1315@gmail.com>
2026-02-24 11:11:05 +0500

    Add files via upload

---
+++ Views/Gait.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMusicDistr.Models
+{
+    public class Gait
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+
+        [ForeignKey("Pair")]
+        public int PairID { get; set; }
+
+        [BindProperty]
+        public ICollection<Pair>? Pairs { get; set; }
+    }
+}
